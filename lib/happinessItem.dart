29a4e7c7ae2646d8123c1e5586fb55fd2e67a169
suyeon2109 <51404948+suyeon2109@users.noreply.github.com>
2022-08-05 19:50:47 +0900

    Revert "글쓰기 화면 gauge 추가, 저장시 디버깅용 SnackBar 추가"

--- lib/happinessItem.dart
+++ lib/happinessItem.dart
@@ -1,19 +1,14 @@
+import 'package:flutter/material.dart';
+
 class Happiness {
   String? title;
   String? content;
-  int? happinessIndex;
+  int? happiness;
   String date;
-  String? imagePath;
 
   Happiness(
       {required this.title,
-      required this.content,
-      required this.happinessIndex,
-      required this.date,
-      this.imagePath});
-
-  @override
-  String toString() {
-    return 'Happiness{title: $title, content: $content, happinessIndex: $happinessIndex, date: $date, imagePath: $imagePath}';
-  }
+        required this.content,
+        required this.happiness,
+        required this.date});
 }
